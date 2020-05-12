8ac20ac2a067b576b6b264489388b621f7f873c8
mac <mac@macs-MBP.fritz.box>
2020-05-12 16:55:46 +0200

    add images at the tshirt

--- second_page/js/script.js
+++ second_page/js/script.js
@@ -7,3 +7,21 @@ $('.s_panel__base_bg__slider').slick({
 
 // 
 
+
+
+
+// let close = document.querySelector('.s_panel__color_label__show');
+// let open = document.querySelector('.tabs');
+// let colorPicker = document.querySelector('.s_panel__color_piker');
+// let slider = document.querySelector('.s_panel__base-dynamic-el')
+
+// open.addEventListener('click', function() {
+//   if(colorPicker.style.display == 'none' || slider.style.display == 'none') {
+//     colorPicker.style.display = 'block';
+//     slider.style.display = 'block';
+//   } else {
+//     colorPicker.style.display = 'none';
+//     slider.style.display = 'none';
+//   }
+
+// });
